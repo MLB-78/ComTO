c3d982bf08da3b533d2be2d674195cabdbaca065
MLB <100378645+MLB-78@users.noreply.github.com>
2023-03-01 18:52:22 +0100

    page d'accueil Mi-Parcours

--- Accueil.php
+++ Accueil.php
@@ -4,9 +4,81 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="Style.css">
+    <link rel="shortcut icon" href="Images/logoComto.png" type="image/x-icon">
     <title>ComTO - Accueil</title>
 </head>
 <body>
     
+<?php include "header.php" ?>
+
+<div class="accueil">
+
+
+
+</div>
+
+<!-- Btn connexion ou inscription avec JS (Guettez script NARUTO)-->
+<div class="bloc">
+
+    <div class="lesBoutons">
+            
+        <div class="btn1">
+            
+            <button type="button" class="btn btn-outline-primary">Connexion</button>
+            
+            
+        </div>
+    
+        &nbsp;&nbsp;
+    
+        <div class="btn2">
+            
+            <button type="button" class="btn btn-outline-warning">Inscription</button>
+            
+          
+        </div>
+
+            
+    </div>        
+   
+</div>
+
+    <br><br>
+
+    
+<div class="bloc2">
+        
+    <h2>Se connecter</h2>
+
+    <br>
+    <!-- les Formulaires  -->
+
+    <form>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="form-group form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    
+            
+</div>
+
+<?php include "Footer.php"; ?>
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
 </body>
 </html>
---
+++ Header.php
@@ -0,0 +1,29 @@
+
+
+
+
+<nav class="navbar navbar-expand-lg navbar-light " style="background-color: whitesmoke;">
+  <a class="navbar-brand" href="Accueil.php"> <img src="Images/logoComToBaleze.png" width="40px"> &nbsp; ComTO &nbsp; <img src="Images/logoComToBaleze.png" width="40px"> </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Accueil </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Histoire</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">TO</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link">Mon Compte</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link">Commander</a>
+      </li>
+    </ul>
+  </div>
+</nav>
